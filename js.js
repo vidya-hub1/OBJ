bd6605986ac6aea01fbad7d5f44a9837efc67f48
shashank boddula <vidyaboddula625@gmail.com>
2024-12-31 22:17:28 +0530

    task

--- js.js
+++ js.js
@@ -2,44 +2,44 @@
 // Access and print the firstName property.
 // Add a new property city with the value "New York" and print the updated object.
 
-// var person = {
+var person = {
     
-//     firstname:"Vidya",
-//     lastname: "Boduula",
-//     age: 21,
-// };
-// console.log(person.firstname)
-// person.city = "New York";
-// console.log(person)
+    firstname:"Vidya",
+    lastname: "Boduula",
+    age: 21,
+};
+console.log(person.firstname)
+person.city = "New York";
+console.log(person)
 
 
 // 2.Create an object product with properties name, price, and inStock. Then:
 // Modify the price property to a new value.
 // Delete the inStock property from the object and print the updated object.
 
-// var product = {
-//     name: "water purifier",
-//     price: 45000,
-//     instock: true,
-// }
-// product.price= 50000;
-// delete product.instock;
-// console.log(product)
+var product = {
+    name: "water purifier",
+    price: 45000,
+    instock: true,
+}
+product.price= 50000;
+delete product.instock;
+console.log(product)
 
 // 3.Create an object library with nested objects for different genres. Each genre has a list of books. Then:
 // Add a new genre scienceFiction with a list of two books.
 // Access and print the first book in the fantasy genre.
 // Delete the mystery genre from the object.
 
-// var library = {
-//     fantasy: ["Harry potter" , "The way of kings"],
-//     mystry: ["The molten city" , "Sherlock Holmes"],
-//     romace: ["The love", "The notebook"],
-// }
-// library.scienceFiction= ["Dune" , "Enders game"];
-// console.log(library.fantasy)
-// delete library.mystry;
-// console.log(library)
+var library = {
+    fantasy: ["Harry potter" , "The way of kings"],
+    mystry: ["The molten city" , "Sherlock Holmes"],
+    romace: ["The love", "The notebook"],
+}
+library.scienceFiction= ["Dune" , "Enders game"];
+console.log(library.fantasy)
+delete library.mystry;
+console.log(library)
 
 
 
@@ -48,24 +48,24 @@
 // Try to change the email property and check if the object is frozen using Object.isFrozen().
 // Add a new property phone to the user object and observe the result.
 
-// var user = {
-//     username: "vidya_123",
-//     email: "vidyaboddulagmail.com" ,
-//     address: {
-//         city: "Hyderadbad",
-//         state: "Telanaga",
-//         zip: 500061,
+var user = {
+    username: "vidya_123",
+    email: "vidyaboddulagmail.com" ,
+    address: {
+        city: "Hyderadbad",
+        state: "Telanaga",
+        zip: 500061,
 
-//     }
+    }
 
 
-// }
-// console.log(user)
-// Object.freeze(user)
-// user.email = "vidya123gmai.com";
-// console.log(Object.isFrozen(user))
-// user.phn = 1234567890;
-// console.log(user)
+}
+console.log(user)
+Object.freeze(user)
+user.email = "vidya123gmai.com";
+console.log(Object.isFrozen(user))
+user.phn = 1234567890;
+console.log(user)
 
 // 5.Create an object car with properties make, model, and price. Then:
 // Seal the object using Object.seal().
@@ -74,16 +74,16 @@
 
 
 
-// var car = {
-//     make: 2000,
-//     model: "shift desier",
-//     price: 1000000,
-// }
-// Object.seal(car)
-// delete car.make;
-// console.log(Object.isSealed(car))
-// car.price= 1110000,
-// console.log(Object.isExtensible(car))
+var car = {
+    make: 2000,
+    model: "shift desier",
+    price: 1000000,
+}
+Object.seal(car)
+delete car.make;
+console.log(Object.isSealed(car))
+car.price= 1110000,
+console.log(Object.isExtensible(car))
 
 
 // 6.Console.log the spinach stock and console .log the hp Pavilion15 price
